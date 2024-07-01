40d0e4ad787255f7c49235540af96f7c1c3df414
moonbeomsu <85485633+moonbeomsu@users.noreply.github.com>
2024-07-01 22:35:06 +0900

    Add files via upload

---
+++ gyungMaeGo/src/test/java/com/example/gyungmaego/GyungMaeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyungmaego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyungMaeGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
